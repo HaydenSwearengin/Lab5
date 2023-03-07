9392ac3644e8db717ee3e466765b9f7f388f2729
HaydenSwearengin <125074327+HaydenSwearengin@users.noreply.github.com>
2023-03-07 11:13:02 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿
+int loops;
+
+Console.WriteLine("How many prices would you like?");
+loops = Convert.ToInt16(Console.ReadLine());
+
+double final_Num = 0;
+double price;
+for(int i = 0;i < loops;i++)
+{
+    Console.Write($"Enter Price {i}: $");
+    price = Convert.ToUInt16(Console.ReadLine());
+    final_Num = final_Num + price;
+}
+
+Console.WriteLine($"The total amount of all prices is: ${final_Num}");
